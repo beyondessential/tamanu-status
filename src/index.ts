dddccb77686fe27ecd8185762bcd8a4682fe86d2
Félix Saparelli <felix@passcod.name>
2022-10-25 13:13:52 +1100

    Get server going

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hellow world')
+import AWS from 'aws-sdk';
+import fastify from 'fastify';
+
+const dyndb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const server = fastify();
+
+server.get('/', async (request, reply) => {
+  return 'pong';
+});
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+
+server.listen({ port }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  console.log(`Server listening at ${address}`)
+});
+
